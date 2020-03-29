a9f6de3be21186ab23b863d391d48a45df2acb66
varubiom1 <rubiom.vane@gmail.com>
2020-03-28 22:52:31 -0500

    Vrubio

---
+++ MyBackTwitter/controllers/user.controller.js
@@ -0,0 +1,32 @@
+const dbManager = require('../database/db.manager');
+
+/**
+ * @param {*} req
+ * @param {*} res
+*/
+
+function createUser (req,res){
+    if (!req.body) {
+        res.status(400).send({
+          message: "REQ ESTÁ VACÍO! OUT OF GAS!:("
+        });
+        return;
+    }
+    const newUserObject ={
+        username: req.body.username,
+        creation_date: req.body.creation_date
+    }
+
+    dbManager.User.create(newUserObject).then (
+        data => {
+            res.send (data);
+        }
+    ).catch (
+        error => {
+           console.log(e);
+           res.status(500).send({
+                message: ":( HUBO UN ERROR"
+            });
+        }
+    );
+}
